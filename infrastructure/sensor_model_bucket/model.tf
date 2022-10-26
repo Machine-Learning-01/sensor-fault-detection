09f5794a2bf3528dcb9a0f236ecd38f936c8c5c1
sethusaim <sethusaim@gmail.com>
2022-10-26 18:56:05 +0530

    modified sensor s3 bucket terraform files

--- infrastructure/sensor_model_bucket/model.tf
+++ infrastructure/sensor_model_bucket/model.tf
@@ -1,4 +1,9 @@
-resource "aws_s3_bucket" "model" {
-  bucket        = var.model
+resource "random_integer" "random" {
+  min = 1
+  max = 50000
+}
+
+resource "aws_s3_bucket" "model_bucket" {
+  bucket        = "${random_integer.random.id}${var.model_bucket_name}"
   force_destroy = var.force_destroy_bucket
 }
