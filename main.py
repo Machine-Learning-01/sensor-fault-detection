5ad238478fa765e3747ddf5132f5689c1f84943d
sethusaim <sethusaim@gmail.com>
2022-10-30 11:52:15 +0530

    removed main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from sensor.pipeline.train_pipeline import TrainPipeline
-
-pipeline = TrainPipeline()
-
-data_ingestion_artifact = pipeline.start_data_ingestion()
-
-data_validation_artifact = pipeline.start_data_validation(data_ingestion_artifact)
